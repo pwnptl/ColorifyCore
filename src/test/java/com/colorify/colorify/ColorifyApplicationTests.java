36ef2fccef98993dee6a3596253064c1700a6af7
pawan <pawaanpatel12895@gmail.com>
2023-01-31 13:38:24 +0530

    Init Spring Rest Project

---
+++ src/test/java/com/colorify/colorify/ColorifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colorify.colorify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
