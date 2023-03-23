3384815c2791201fe21f4a18fc0135dc24b0d8b0
pawan <pawanpatel12895@gmail.com>
2023-03-23 12:50:43 +0530

    Changed Naming of MessageHandlerTypes

--- src/main/java/com/platform/core/registry/messageHandler/MessageHandlerType.java
+++ src/main/java/com/platform/core/registry/messageHandler/MessageHandlerType.java
@@ -1,24 +1,28 @@
 package com.platform.core.registry.messageHandler;
 
+import lombok.Getter;
+
 public enum MessageHandlerType {
 
     // Requests -> should not require handler if explicit Response type is present. todo : simplify this incompetence.
     GET_PLAYER_DATA("GET_PLAYER_DATA"),
-
+    SYN("SYN"),
 
     // Responses
     PLAYER_DATA("PLAYER_DATA"),
+    ACK("ACK"),
 
 
     // Misc
-    START_BUTTON_MESSAGE_HANDLER("DATA_HANDLER"),
-    UNKNOWN("IDK"),
+    START_BUTTON_MESSAGE_HANDLER("    START_BUTTON_MESSAGE_HANDLER(\"DATA_HANDLER\"),\n"),
+    UNKNOWN("UNKNOWN"),
     DEFAULT("DEFAULT")
     ;
 
     /*
     * Value of the enum.
     * */
+    @Getter
     private final String value;
 
     MessageHandlerType(String value) {
