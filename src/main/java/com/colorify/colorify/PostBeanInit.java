1316ff012a385ed6507d10af79b75f2b725f0afd
pawan <pawanpatel12895@gmail.com>
2023-03-25 18:08:28 +0530

    Added JoinGameMessageHandler

--- src/main/java/com/colorify/colorify/PostBeanInit.java
+++ src/main/java/com/colorify/colorify/PostBeanInit.java
@@ -34,6 +34,7 @@ public class PostBeanInit implements InitializingBean {
         messageHandlerRegistry.put(MessageHandlerType.GET_PLAYER_DATA, playerFacade.getGetPlayerRequestHandler());
         messageHandlerRegistry.put(MessageHandlerType.CREATE_PLAYER, playerFacade.getCreatePlayerRequestHandler());
         messageHandlerRegistry.put(MessageHandlerType.CREATE_GAME, gameFacade.getCreateGameMessageHandler());
+        messageHandlerRegistry.put(MessageHandlerType.JOIN_GAME, gameFacade.getJoinGameMessageHandler());
         messageHandlerRegistry.put(MessageHandlerType.DEFAULT, new MessageHandlerInterface() {
             @Override
             public void handleMessage(String sessionId, String message) throws IOException {
