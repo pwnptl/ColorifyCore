7ee377467bdd78a1b496bf337f81abcdcf7c3498
pawan <pawanpatel12895@gmail.com>
2023-04-08 10:30:08 +0530

    Added Websocet helper

---
+++ src/main/java/com/platform/core/network/WebSocketHandlerHelper.java
@@ -0,0 +1,31 @@
+package com.platform.core.network;
+
+import com.platform.core.utility.Logger;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class WebSocketHandlerHelper {
+
+    private final SessionsManager sessionsManager;
+
+    WebSocketHandlerHelper() {
+        this.sessionsManager = SessionsManager.getInstance();
+    }
+
+    public void broadcastMessageByPlayerIds(ArrayList<String> playerIds, String message) {
+        for (String playerId : playerIds) {
+            String sessionId = sessionsManager.findSessionIdByPlayerId(playerId);
+            WebSocketSession session = sessionsManager.get(sessionId);
+            try {
+                session.sendMessage(new TextMessage(message));
+            } catch (IOException e) {
+                Logger.error("exception caught in broadcasting to player " + playerId);
+                Logger.error(e.getMessage());
+                // throw new RuntimeException(e); // todo: broadcast using queues and threads with retryables.
+            }
+        }
+    }
+}
