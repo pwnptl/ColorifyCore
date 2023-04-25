1072d8634d43a22396abd48091f69811c6f44769
pawan <pawanpatel12895@gmail.com>
2023-04-26 00:07:33 +0530

    Improvised WebSocketHandlerHelper by making payload inside the class rather than in consumers.

--- src/main/java/com/platform/core/network/WebSocketHandlerHelper.java
+++ src/main/java/com/platform/core/network/WebSocketHandlerHelper.java
@@ -17,11 +17,6 @@ public class WebSocketHandlerHelper {
         this.sessionsManager = SessionsManager.getInstance();
     }
 
-    public void broadcastMessageByPlayerIds(Set<String> playerIds, String message) {
-        for (String playerId : playerIds) {
-            sendMessageByPlayerId(playerId, message);
-        }
-    }
 
     public void broadcastMessageByPlayerIds(Set<String> playerIds, MessageHandlerType messageHandlerType, Response response) {
         for (String playerId : playerIds) {
@@ -31,20 +26,14 @@ public class WebSocketHandlerHelper {
 
     public void sendMessageByPlayerId(String playerId, MessageHandlerType messageHandlerType, Response response) {
         try {
-            sessionsManager.findSessionByPlayerId(playerId).sendMessage(getPayloadTextMessage(messageHandlerType, response));
+
+            WebSocketSession session = sessionsManager.findSessionByPlayerId(playerId);
+            if (session == null)
+                Logger.error("session Id not found for playerId " + playerId);
+            else
+                session.sendMessage(getPayloadTextMessage(messageHandlerType, response));
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private void sendMessageByPlayerId(String playerId, String message) {
-        try {
-            WebSocketSession session = sessionsManager.findSessionByPlayerId(playerId);
-            session.sendMessage(new TextMessage(message));
-        } catch (IOException e) {
-            Logger.error("exception caught in broadcasting to player " + playerId);
-            Logger.error(e.getMessage());
-            // throw new RuntimeException(e); // todo: broadcast using queues and threads with retryables.
         }
     }
 
