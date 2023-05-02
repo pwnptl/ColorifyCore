788a8a7287436bdf0377fc2e69d18d90670bb4a3
pawan <pawanpatel12895@gmail.com>
2023-05-02 19:31:02 +0530

    Added ScoreTrackerTypeAdapter and ScoreTracker

--- src/main/java/com/platform/core/game/Score.java
+++ src/main/java/com/platform/core/game/Score.java
@@ -1,13 +1,14 @@
 package com.platform.core.game;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
+@AllArgsConstructor
 public class Score {
 
     private int count;
 
-    public Score(int count) {
-        this.count = count;
-    }
 }
--- src/main/java/com/platform/core/game/ScoreTracker.java
+++ src/main/java/com/platform/core/game/ScoreTracker.java
@@ -1,8 +1,10 @@
 package com.platform.core.game;
 
+import com.colorify.game.utilities.GameConfiguration;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Getter
@@ -11,8 +13,18 @@ public abstract class ScoreTracker {
     protected Map<String, Score> playerIdToScoreMap;
     protected int totalCells;
 
-    public void addScore(String playerId, Score score) {
-        playerIdToScoreMap.put(playerId, score);
+    public ScoreTracker(GameConfiguration gameConfiguration) {
+        playerIdToScoreMap = new HashMap<>(gameConfiguration.getPlayerCount());
+        totalCells = gameConfiguration.getRows() * gameConfiguration.getColumns();
+    }
+
+    public ScoreTracker() {
+        playerIdToScoreMap = new HashMap<>();
+        totalCells = 0;
+    }
+
+    public void updateScore(String playerId, int coveredCells) {
+        playerIdToScoreMap.getOrDefault(playerId, new Score(0)).setCount(coveredCells);
     }
 
     public float getPercentScoreByPlayer(String playerId) {
