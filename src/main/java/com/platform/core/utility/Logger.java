1126dd3d026dde5707cf8996fcc25e35248b9226
pawan <pawanpatel12895@gmail.com>
2023-05-09 14:30:35 +0530

    improvised Logger

--- src/main/java/com/platform/core/utility/Logger.java
+++ src/main/java/com/platform/core/utility/Logger.java
@@ -2,19 +2,40 @@ package com.platform.core.utility;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Logger {
+    static List<String> logsToShow = new ArrayList<>();
+    static List<String> logsNotToShow = new ArrayList<>();
+
+    static {
+        logsNotToShow.add("com.platform.core.database.MongoDB");
+        logsNotToShow.add("com.colorify.colorify.PostBeanInit");
+        logsNotToShow.add("com.colorify.colorify.controller.WebController");
+        logsNotToShow.add("com.colorify.colorify.controller.GameController");
+        logsNotToShow.add("com.colorify.colorify.controller.PlayerController");
+
+    }
 
     public static void info(String log, Object... objs) {
         ArrayList<String> strings = new ArrayList<>();
-        for(Object o : objs)
-        {
-            strings.add(objs.toString());
-        }
+        if (objs != null)
+            for (Object o : objs) {
+                strings.add(objs.toString());
+            }
         System.out.println(log + " " + Arrays.toString(new ArrayList[]{strings}));
     }
+
     public static void info(String log, String str) {
-        System.out.println(log + " : " + str);
+        if (logsNotToShow.contains(log))
+            return;
+        else if (logsToShow.contains(log)) {
+            System.out.print(log + " : ");
+            info(str);
+        } else {
+            System.out.print(log + " : ");
+            info(str);
+        }
     }
 
     public static void test(String log) {
