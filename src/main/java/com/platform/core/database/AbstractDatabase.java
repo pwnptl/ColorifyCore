67d9d3975b9046b79c6176a6e9470e29e01aed08
pawan <pawanpatel12895@gmail.com>
2023-11-14 00:08:20 +0530

    Add MapDB impl

--- src/main/java/com/platform/core/database/AbstractDatabase.java
+++ src/main/java/com/platform/core/database/AbstractDatabase.java
@@ -1,54 +1,30 @@
 package com.platform.core.database;
 
-import com.colorify.game.mechanics.BaseGame;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.platform.core.game.AbstractBaseGame;
-import com.platform.core.player.Player;
-
-import java.util.Map;
-
 public abstract class AbstractDatabase {
-
-    protected Gson gson;
     protected static AbstractDatabase instance;
 
     protected AbstractDatabase() {
+        // Initialization code, if needed
     }
 
-    protected AbstractDatabase(final Map<Class, Object> typeAdapters) {
-        initializeGsonWithTypeAdapters(typeAdapters);
-    }
-
-    public static AbstractDatabase getInstance(final Map<Class, Object> typeAdapters) {
-        if (instance == null)
-            instance = new InMemoryStorage(); // todo: initiate via Config.
+    public static synchronized AbstractDatabase getInstance() {
+        if (instance == null) {
+            instance = createInstance();
+        }
         return instance;
     }
-    protected void initializeGsonWithTypeAdapters(final Map<Class, Object> typeAdapters) {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        for (Map.Entry<Class, Object> pair : typeAdapters.entrySet()) {
-            gsonBuilder.registerTypeHierarchyAdapter(pair.getKey(), pair.getValue());
-        }
-        gson = gsonBuilder.create();
+
+    protected abstract void addCollection(String name);
+    protected abstract void create(String collectionName, String key, Object value);
+    protected abstract Object read(String collectionName, String key);
+    protected abstract void update(String collectionName, String key, Object value);
+    protected abstract void delete(String collectionName, String key);
+    protected abstract void reset(String collectionName);
+    protected abstract void close();
+
+    private static AbstractDatabase createInstance() {
+        // You can choose the concrete implementation to instantiate here
+        // For simplicity, let's assume MapDB as the implementation
+        return new MapDB();
     }
-
-    public abstract void init();
-
-    // scalability issue ??? -> can we segregate MODELS from DB_OPERATIONS ??
-    public abstract boolean putPlayer(final String id, final Player data);
-
-    public abstract boolean putGame(final String id, final AbstractBaseGame data);
-
-    public abstract Player queryPlayer(final String id);
-
-    public abstract AbstractBaseGame queryGame(final String id, Class<BaseGame> baseGameClass);
-
-    public abstract boolean updatePlayer(String gameId, BaseGame game);
-
-    public abstract boolean updateGame(String gameId, BaseGame game);
-
-    public abstract boolean deletePlayer(final String id);
-
-    public abstract boolean deleteGame(final String id);
 }
---
+++ src/main/java/com/platform/core/database/MapDB.java
@@ -0,0 +1,79 @@
+package com.platform.core.database;
+
+import com.platform.core.utility.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class MapDB extends AbstractDatabase {
+    private final DB db;
+    private final Map<String, Map<String, Object>> dataMaps;
+
+    public MapDB() {
+        super();
+        db = DBMaker.fileDB(Constants.DBConstants.DB_NAME).make();
+        dataMaps = new HashMap<>();
+    }
+
+    // Singleton instance creation method
+    public static synchronized AbstractDatabase getInstance() {
+        if (instance == null) {
+            instance = new MapDB();
+        }
+        return instance;
+    }
+
+    @Override
+    protected void addCollection(String name) {
+        HTreeMap collection = db.hashMap(name, Serializer.STRING, Serializer.JAVA).createOrOpen();
+        dataMaps.put(name, collection);
+    }
+
+    @Override
+    protected void create(String collectionName, String key, Object value) {
+        try {
+            Map<String, Object> collection = dataMaps.get(collectionName);
+            collection.put(key, value);
+            db.commit();
+        } catch (NullPointerException npe) {
+            // todo: add log
+            throw new NullPointerException();
+        }
+    }
+
+    @Override
+    protected Object read(String collectionName, String key) {
+        Map<String, Object> collection = dataMaps.get(collectionName);
+        log.info("collection " + collectionName + " have : " + collection);
+        return collection.get(key);
+    }
+
+    @Override
+    protected void update(String collectionName, String key, Object value) {
+    }
+
+    @Override
+    protected void delete(String collectionName, String key) {
+    }
+
+    @Override
+    protected void reset(String collectionName) {
+        Map<String, Object> collection = dataMaps.get(collectionName);
+        collection.clear();
+        db.commit();
+    }
+
+
+    @Override
+    protected void close() {
+        if (db != null && !db.isClosed()) {
+            db.close();
+        }
+    }
+}
