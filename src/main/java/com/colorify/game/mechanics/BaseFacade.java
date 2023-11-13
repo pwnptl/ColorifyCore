b5450647fe4d128631147749d82b58a92a38f640
pawan <pawanpatel12895@gmail.com>
2023-11-14 00:08:45 +0530

    Remove TypeAdapters

--- src/main/java/com/colorify/game/mechanics/BaseFacade.java
+++ src/main/java/com/colorify/game/mechanics/BaseFacade.java
@@ -1,18 +1,4 @@
 package com.colorify.game.mechanics;
 
-import com.colorify.game.mechanics.typeAdapters.BoardCellTypeAdapter;
-import com.colorify.game.mechanics.typeAdapters.ScoreTrackerTypeAdapter;
-import com.platform.core.game.Cell;
-import com.platform.core.game.ScoreTracker;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class BaseFacade {
-    protected Map<Class, Object> getTypeAdapters(){
-        Map<Class, Object> typeAdapters = new HashMap<>();
-        typeAdapters.put(Cell.class, new BoardCellTypeAdapter());
-        typeAdapters.put(ScoreTracker.class, new ScoreTrackerTypeAdapter());
-        return typeAdapters;
-    }
 }
